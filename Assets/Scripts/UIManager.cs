f9b9a7d8fc1b362e9c5b8680a568a223775f1570
360808-afk <360808@nptcgroup.ac.uk>
2025-12-08 10:13:13 +0000

    Initial commit

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text _scoreText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+    }
+
+}
